10ddbf4c326b71f53190fd77b80fdc5278fc4d3d
Ritikraja13 <115801193+rajasingh05@users.noreply.github.com>
2023-10-13 20:47:14 +0000

    js

---
+++ javaScript/array_practice1.js
@@ -0,0 +1,11 @@
+let arr = [1, true, "Ritik", 0.5,3 ];
+console.log(arr);
+
+let b = new Array("abc", "def", "ghi");
+console.log(b);
+
+// to find the position of an array.
+
+console.log(arr[4]);
+arr[4] = 5;
+console.log(arr);
