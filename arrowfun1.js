0fbe2a42cd26776613e17a210c2e455fd76d8772
Ritikraja13 <115801193+rajasingh05@users.noreply.github.com>
2023-10-14 20:04:49 +0000

    js

---
+++ arrowfun1.js
@@ -0,0 +1,13 @@
+const square = x => x*x;
+
+// const sumOftwoNumber = (x,y) => x + y;
+
+// const sum = (x,y) => {
+//     console.log(`Adding ${x} and ${y}`);
+//     return x + y;
+// };
+
+// const sumAndDifference = (x,y) => ({ sum: x + y, difference: x - y});
+
+// console.log(square(7));
+square(8);
